e0dfeb7e631c601121348c2ff4811bdd79557a93
Syafa Adena <gvoze32@gmail.com>
2022-04-19 22:23:28 +0700

    Update

--- app/src/main/java/com/example/aplikasi_responsi/RegisterActivity.kt
+++ app/src/main/java/com/example/aplikasi_responsi/RegisterActivity.kt
@@ -2,10 +2,17 @@ package com.example.aplikasi_responsi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        supportActionBar?.hide()
+    }
+
+    fun registerBtn(view: View) {
+        val intent = android.content.Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
