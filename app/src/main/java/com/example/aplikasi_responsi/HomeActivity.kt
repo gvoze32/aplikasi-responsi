f88cab48573a7e2a19bf4103b8b1fa736e1f717e
Syafa Adena <gvoze32@gmail.com>
2022-04-20 00:34:36 +0700

    giving up using viewmodel, i use binding then..

--- app/src/main/java/com/example/aplikasi_responsi/HomeActivity.kt
+++ app/src/main/java/com/example/aplikasi_responsi/HomeActivity.kt
@@ -4,39 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.example.aplikasi_responsi.databinding.ActivityHomeBinding
 import com.example.aplikasi_responsi.history.HistoryFragment
 import com.example.aplikasi_responsi.home.HomeFragment
 import com.example.aplikasi_responsi.payment.PaymentFragment
 import com.example.aplikasi_responsi.setting.SettingFragment
 
 class HomeActivity : AppCompatActivity() {
-
+    private var _binding: ActivityHomeBinding? = null
+    private val binding get() = _binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        _binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
         supportActionBar?.hide()
-
-}
-
-    fun btnHome(view: ImageView) {
-        loadFragment(HomeFragment())
-    }
-
-    fun btnHistory(view: ImageView) {
-        loadFragment(HistoryFragment())
-    }
-
-    fun btnPayment(view: ImageView) {
-        loadFragment(PaymentFragment())
-    }
-
-    fun btnSetting(view: ImageView) {
-        loadFragment(SettingFragment())
+        binding?.apply {
+            btnHome.setOnClickListener {
+                loadFragment(HomeFragment())
+            }
+        }
+        binding?.btnHistory?.setOnClickListener {
+            loadFragment(HistoryFragment())
+        }
+        binding?.btnPayment?.setOnClickListener {
+            loadFragment(PaymentFragment())
+        }
+        binding?.btnSetting?.setOnClickListener {
+            loadFragment(SettingFragment())
+        }
     }
 
     private fun loadFragment(fragment: Fragment) {
-    supportFragmentManager.beginTransaction()
-        .replace(R.id.container, fragment)
-        .commitNow()
-}
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commitNow()
+    }
 }
